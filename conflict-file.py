42bf16cfe3d95aac0b1863c84c5ac564cf32ff9f
mil-2547 <artiprice@gmail.com>
2025-09-15 19:47:37 +0300

    added&changed: conflict-file.py, README.md" Try to solve conflicts on your own

---
+++ conflict-file.py
@@ -0,0 +1,6 @@
+# conflict-file.py
+
+def multiply(x, y):
+    if !isinstance(x, int or float) and !isinstance(y, int or float):
+        raise Exception("The type of x and y have to be int or float!!!");
+    return x*y
